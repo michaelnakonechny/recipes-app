01599bf853a2047c2a098d211075e0c6fa06a8f1
Michael Nakonechny <michaelnako@icloud.com>
2022-09-06 12:42:41 +0200

    footer-header

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Veggie</li>
+          <li>Desserts</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
