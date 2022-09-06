a5573819c90fcd56859a7bebc4e6fa17f22cf1ee
Michael Nakonechny <michaelnako@icloud.com>
2022-09-06 12:47:06 +0200

    bugfix

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>About us</li>
+        <li>Legal Notice</li>
+        <li>Contact</li>
+      </ul>
+    </footer>
+  );
+}
