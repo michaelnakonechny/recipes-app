07ad4cef688c2abfae22ada47a4d8ae90a5d2b5f
Michael Nakonechny <michaelnako@icloud.com>
2022-09-06 12:32:02 +0200

    pages

---
+++ pages/dessert.js
@@ -0,0 +1,8 @@
+export default function Desserts() {
+  return (
+    <>
+      <h1>Desserts</h1>
+      <p>Pudding</p>
+    </>
+  );
+}
---
+++ pages/veggie.js
@@ -0,0 +1,8 @@
+export default function Veggie() {
+  return (
+    <>
+      <h1>Veggie Menu</h1>
+      <p>Paprika</p>
+    </>
+  );
+}
